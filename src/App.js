8d37f14f4168dd481ab3be11e86280d51a9c622a
Earts-Likhitha <likhitha.t@entitledarts.com>
2023-11-15 17:57:45 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+} from "react-router-dom";
+import "./App.css"
+import Home from "./components/Home";
+import Navbar from "./components/Navbar";
+import Destination from "./components/Destination";
+import Contact from "./components/Contact";
+import Blogs from "./components/Blogs";
+import Locations from "./components/Locations";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/destination" element={<Destination />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="/blogs" element={<Blogs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from './Navbar'
+const Home = () => {
+  return (
+    <>
+    <div className='container-fluid'>
+    <img  src="assets/banner1.png" className="img-fluid w-100 h-75" alt="image" />
+
+    </div>
+    </>
+  )
+}
+
+export default Home
